6c3a2f8039dd7c73e870afe6ecc53997e731bf9b
LiuzF-05 <luizfsilvacs@gmail.com>
2025-06-14 14:45:13 -0300

    updated the static site generator to be compatible with GitHub Pages

--- src/main.py
+++ src/main.py
@@ -3,8 +3,13 @@ from src.htmlnode import HTMLNode, LeafNode, ParentNode
 from src.converter import *
 from src.splitter import *
 from src.site_generating import copy_to_public, generate_page, generate_pages_recursive
+import sys
 
 def main():
-  copy_to_public("./static","./public")
-  generate_pages_recursive("./content/","./template.html", "./public/")
+  basepath="/"
+  if len(sys.argv)>1:
+    basepath=sys.argv[1]
+  print(basepath)
+  copy_to_public("./static","./docs")
+  generate_pages_recursive("./content","./template.html", "./docs", basepath)
 main()
--- src/site_generating.py
+++ src/site_generating.py
@@ -20,7 +20,7 @@ def copy_to_public(source, destination, cleaned=False):
             copy_to_public(file_path, new_dir, cleaned=False)
     return
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     if not os.path.exists(from_path):
         raise Exception("invalid origin file path")
@@ -30,27 +30,25 @@ def generate_page(from_path, template_path, dest_path):
     title=extract_title(from_text)
     with open(template_path) as t:
         template_text=t.read()
-        template_text=template_text.replace("{{ Content }}", content).replace("{{ Title }}", title)
+        template_text=template_text.replace("{{ Content }}", content).replace("{{ Title }}", title).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     with open(dest_path, "x") as d:
         d.write(template_text)
     return 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for file in os.listdir(dir_path_content):
         file_path=os.path.join(dir_path_content, file)
         if os.path.isfile(file_path):
-            #print(file_path)
             new_file=change_file_extensions(file, ".html")
             dest_file=os.path.join(dest_dir_path, new_file)
-            generate_page(file_path, template_path, dest_file)
+            generate_page(file_path, template_path, dest_file, basepath)
         else:
             new_dir=os.path.join(dest_dir_path, file)
             os.mkdir(new_dir)
-            generate_pages_recursive(file_path, template_path, new_dir)
+            generate_pages_recursive(file_path, template_path, new_dir, basepath)
         
 def change_file_extensions(old, extension):
     base_name=os.path.splitext(old)
-    print(base_name)
     if base_name[1]==".md":
         return base_name[0]+extension
     else:
